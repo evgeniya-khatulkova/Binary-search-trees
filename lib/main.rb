53a1940ece317f4bf63a50579a485559bc25db6c
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-06-05 11:15:39 +0200

    create a tree class

--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,4 @@
 require_relative 'node'
-# require_relative 'tree'
+require_relative 'tree'
  p Node.new(2)
+ p Tree.new([1, 5, 6])
---
+++ lib/tree.rb
@@ -0,0 +1,9 @@
+class Tree
+
+  attr_accessor :arr, :root
+
+  def initialize(arr)
+    @arr = arr
+    @root = nil
+  end
+end
